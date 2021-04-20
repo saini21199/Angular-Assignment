69463a82b3a56f75645ca039117fdbe7e3540b8d
saini21199 <sachinsainiandromeda007@gmail.com>
2021-04-20 19:07:02 +0530

    first commit

---
+++ product-catalog/src/app/product/product.ts
@@ -0,0 +1,9 @@
+export class Product {
+    Id: number;
+    Name: string;
+    Manufacturer: string;
+    Code: string;
+    Category: string;
+    Description: string;
+    SellingPrice: number;
+}
